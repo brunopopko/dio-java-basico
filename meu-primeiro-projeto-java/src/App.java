e33418cb25f2c7e92d9c48f2adef84c745609cee
Bruno Popko <bruno.popko@gmail.com>
2022-11-25 12:25:38 -0300

    exemplos anatomia classes

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,18 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Boas Vindas, Turma DIO!");
+    
+    public static void main(String[] args) {
+        
+        String primeiroNome = "Bruno";
+        String segundoNome = "Popko";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
     }
+
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+
+}
+    
 }
